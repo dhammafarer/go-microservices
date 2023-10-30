08fb795fd4dbdfd6c0ae1d42399eb46444b0c078
Pale Kiwi <contact@palekiwi.com>
2023-10-30 14:31:17 +0800

    add db connection

--- project/pod.sh
+++ project/pod.sh
@@ -2,10 +2,30 @@
 
 pod_name=microservices
 
-podman pod create --name $pod_name \
-    -p 8080:8080
+pgsql_image="docker.io/library/postgres:14.2"
+
+podman pod create --replace --name $pod_name \
+    -p 8080:8080 \
+    -p 8280:8280 \
+    -p 5432:5432
 
 podman create --name broker-service \
     --pod $pod_name \
     --restart always \
     microservices/broker-service
+
+podman create --name authentication-service \
+    --pod $pod_name \
+    --restart always \
+    -e DSN="host=127.0.0.1 port=5432 user=postgres password=password \
+    dbname=users sslmode=disable timezone=UTC connect_timeout=5" \
+    microservices/authentication-service
+
+podman create --name postgresql \
+    --pod $pod_name \
+    --restart always \
+    -e POSTGRES_USER="postgres" \
+    -e POSTGRES_PASSWORD="password" \
+    -e POSTGRES_DB="users" \
+    -v ./db-data/postgres/:/var/lib/postgresql/data/:Z \
+    $pgsql_image
--- project/run-broker-service.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-podman run --rm -d --name broker-service \
-    -p 8080:8080 \
-    microservices/broker-service
